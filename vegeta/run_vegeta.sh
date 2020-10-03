85631403a0fa3377fba3ddc49e85c1aedbd6bbfa
Takayoshi SHIRAKI <takayoshi.shiraki@pmtech.co.jp>
2020-10-03 18:30:39 +0900

    fix run_vegeta.sh

--- vegeta/run_vegeta.sh
+++ vegeta/run_vegeta.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-docker run --rm -i peterevans/vegeta sh -c "echo 'GET http://172.20.32.72:5000/api/account' | vegeta attack -rate=10 -duration=180s | tee results.bin | vegeta report -type='hist[0,5ms, 10ms, 15ms, 20ms]'"
+docker run --rm -it vegeta -c "echo 'GET http://gw:5000/api/account' | vegeta attack -rate=10 -duration=10s | tee results.bin | vegeta report -type='hist[0,5ms, 20ms, 40ms, 60ms]'"
